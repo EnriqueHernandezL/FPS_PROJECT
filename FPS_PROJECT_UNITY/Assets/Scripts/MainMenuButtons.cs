9d3eed0b1b46076d01201f256415d92088799ecf
Enrique Hernández L <enrique.hl.7@hotmail.com>
2019-09-25 12:40:31 +0200

    Added MainMenu

---
+++ FPS_PROJECT_UNITY/Assets/Scripts/MainMenuButtons.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuButtons : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1);//loads main level
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();//closes game
+    }
+
+
+}
